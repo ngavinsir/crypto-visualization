98116888daaa38d691529d43279fbcc603b559e9
ngavinsir <ngavinsir@gmail.com>
2020-09-30 19:39:58 +0700

    change copy button

--- src/components/Visualize.svelte
+++ src/components/Visualize.svelte
@@ -83,7 +83,7 @@
 
 <div class={clsx(className, 'flex flex-col w-full')}>
   <div
-    class={clsx('flex self-end items-center space-x-3', {
+    class={clsx('flex self-end items-center space-x-5', {
       invisible: !text.length,
       visible: text.length,
     })}>
@@ -91,20 +91,16 @@
       disabled={!done}
       class="text-white disabled:opacity-25 disabled:cursor-not-allowed"
       on:click={reset}>restart</button>
-    <svg
+    <button
+      disabled={!done}
+      class="text-white disabled:opacity-25 disabled:cursor-not-allowed"
       on:click={() => {
         const app = new CopyClipBoard({
           target: document.getElementById('clipboard'),
           props: { name: currentText.map((t) => t.text).join('') },
         });
         app.$destroy();
-      }}
-      class="w-6 h-6 text-gray-200 cursor-pointer"
-      fill="currentColor"
-      viewBox="0 0 20 20"
-      xmlns="http://www.w3.org/2000/svg"><path
-        d="M7 9a2 2 0 012-2h6a2 2 0 012 2v6a2 2 0 01-2 2H9a2 2 0 01-2-2V9z" />
-      <path d="M5 3a2 2 0 00-2 2v6a2 2 0 002 2V5h8a2 2 0 00-2-2H5z" /></svg>
+      }}>copy</button>
   </div>
   <div class="flex h-8 mt-2 flex-wrap">
     {#each currentText as i, index}
